f7ed0bc05334e93216305de7c6f5b10c73991db4
Guilherme Fernandes <gf_engquimica@hotmail.com>
2022-09-17 15:21:25 -0300

    feat: conecta a aplicação mobile com a api local, implementa rotas e criação da página de games generica

---
+++ mobile/src/screens/Game/index.tsx
@@ -0,0 +1,73 @@
+import { useEffect, useState } from 'react';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { useNavigation, useRoute } from '@react-navigation/native';
+import { FlatList, Image, TouchableOpacity, View, Text } from 'react-native';
+import { Entypo } from '@expo/vector-icons';
+import { GameParams } from '../../@types/navigation';
+
+import { Background } from '../../components/Background';
+import logoImg from '../../assets/logo-nlw-esports.png';
+import { THEME } from '../../theme';
+
+import { styles } from './styles';
+import { Header } from '../../components/Header';
+import { DuoCard, DuoCardProps } from '../../components/DuoCard';
+
+export function Game() {
+  const route = useRoute();
+  const [duos, setDuos] = useState<DuoCardProps[]>([]);
+  const game = route.params as GameParams;
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    fetchAds();
+  }, []);
+
+  const fetchAds = async () => {
+    const response = await fetch(`http://192.168.68.114:3333/games/${game.id}/ads`);
+    const data = await response.json();
+    setDuos(data);
+
+  };
+
+  const handleGoBack = () => {
+    navigation.goBack();
+  };
+  
+  return (
+    <Background>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.header}>
+          <TouchableOpacity onPress={handleGoBack}>
+            <Entypo  name="chevron-thin-left" color={THEME.COLORS.CAPTION_300} size={20} />
+          </TouchableOpacity>
+          
+          <Image source={ logoImg } style={styles.logo} />
+
+          <View style={styles.right} />
+        </View>
+
+        <Image source={{uri: game.bannerUrl}} style={styles.cover} resizeMode="cover" />
+
+        <Header
+          title={game.title} 
+          subtitle="Conecte-se e comece a jogar!"
+        />
+
+        <FlatList
+          data={duos}
+          keyExtractor={item => item.id}
+          renderItem={({ item }) => <DuoCard data={item} onConnect={ () => console.log('modal') } />}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          contentContainerStyle={duos.length > 0 ? styles.contentList : styles.emptyListContent}
+          style={styles.containerList}
+          ListEmptyComponent={ () => <Text style={styles.emptyListText}>
+            Não há anúncios publicados ainda!
+          </Text> }
+        />
+
+      </SafeAreaView>
+    </Background>
+  );
+}
--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -1,29 +1,60 @@
-import { View, Image, FlatList } from 'react-native';
+import { useEffect, useState } from 'react';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { Image, FlatList } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
-import { styles } from './styles';
-import logoImg from '../../assets/logo-nlw-esports.png';
 import { Header } from '../../components/Header';
 import { GameCard } from '../../components/GameCard';
-import { GAMES } from '../../utils/games';
+
+import logoImg from '../../assets/logo-nlw-esports.png';
+
+import { styles } from './styles';
+import { Background } from '../../components/Background';
+
+interface Game {
+  id: string;
+  title: string;
+  bannerUrl: string;
+  _count: { ads:number };
+}
 
 export function Home() {
+  const [games, setGames] = useState<Game[]>([]);
+
+  useEffect(() => {
+    fetchGames();
+  }, []);
+
+  const fetchGames = async () => {
+    const response = await fetch('http://192.168.68.114:3333/games');
+    const data = await response.json();
+    
+    setGames(data);
+  };
+
+  const navigation = useNavigation();
+
+  const handleOpenGame = ({ id, title, bannerUrl }: Game) => {
+    navigation.navigate('game', { id, title, bannerUrl });
+  };
+
   return (
-    <View style={styles.container}>
-      <Image source={ logoImg } style={styles.logo} />
-      <Header 
-        title="Encontre seu duo!" 
-        subtitle="Selecione o game que deseja jogar..."
-      />
-
-      <FlatList
-        data={GAMES}
-        keyExtractor={item => item.id}
-        renderItem={({ item }) => <GameCard data={item} />}
-        horizontal
-        showsHorizontalScrollIndicator={false}
-        contentContainerStyle={styles.contentList}
-      />
-
-    </View>
+    <Background>
+      <SafeAreaView style={styles.container}>
+        <Image source={ logoImg } style={styles.logo} />
+        <Header 
+          title="Encontre seu duo!" 
+          subtitle="Selecione o game que deseja jogar..."
+          />
+        <FlatList
+          data={games}
+          keyExtractor={item => item.id}
+          renderItem={({ item }) => <GameCard data={item} onPress={ () => handleOpenGame(item) } />}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          contentContainerStyle={styles.contentList}
+          />
+      </SafeAreaView>
+    </Background>
   );
 }
